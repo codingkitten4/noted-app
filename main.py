c32c38717fa5ecdfb08eef8c4bb08abf550619a7
codingkitten4 <saudkhan10@hotmail.com>
2024-12-03 21:52:55 -0500

    update ui (unsuccessful) to make all text show in one place

--- main.py
+++ main.py
@@ -31,22 +31,31 @@ class NotedApp(ctk.CTk):
 
     def summarize_image_display(self):
         if self.img:
+            self.summarize_label.grid()
             summarize_img_text = summarize_image(self.img)
             self.summarize_label.configure(text=summarize_img_text)
+            self.question_label.grid_forget()
+            self.text_label.grid_forget()
         else:
             print("Error: No image to summarize.")
 
     def make_questions_display(self):
         if self.img:
+            self.question_label.grid()
             questions_img_text = make_questions(self.img)
             self.question_label.configure(text=questions_img_text)
+            self.summarize_label.grid_forget()
+            self.text_label.grid_forget()
         else:
             print("Error: No image to generate questions from.")
 
     def convert_to_text_display(self):
         if self.img:
+            self.text_label.grid()
             return_img_text = return_text(self.img)
             self.text_label.configure(text=return_img_text)
+            self.question_label.grid_forget()
+            self.summarize_label.grid_forget()
         else:
             print("Error: No image to convert to text.")
 
@@ -55,14 +64,14 @@ class NotedApp(ctk.CTk):
         import_btn = ctk.CTkButton(self, text="take picture", command=self.import_image_display)
         import_btn.grid(row=0, column=0, padx=10, pady=10)
 
-        summarize_btn = ctk.CTkButton(self, text="summarize", command=self.summarize_image_display)
-        summarize_btn.grid(row=1, column=0, padx=10, pady=10)
+        self.summarize_btn = ctk.CTkButton(self, text="summarize", command=self.summarize_image_display)
+        self.summarize_btn.grid(row=1, column=0, padx=10, pady=10)
 
-        questions_btn = ctk.CTkButton(self, text="questions", command=self.make_questions_display)
-        questions_btn.grid(row=2, column=0, padx=10, pady=10)
+        self.questions_btn = ctk.CTkButton(self, text="questions", command=self.make_questions_display)
+        self.questions_btn.grid(row=2, column=0, padx=10, pady=10)
 
-        text_btn = ctk.CTkButton(self, text="convert", command=self.convert_to_text_display)
-        text_btn.grid(row=3, column=0, padx=10, pady=10)
+        self.text_btn = ctk.CTkButton(self, text="convert", command=self.convert_to_text_display)
+        self.text_btn.grid(row=3, column=0, padx=10, pady=10)
 
         self.summarize_label = ctk.CTkLabel(self, text="", fg_color="transparent", wraplength=300)
         self.summarize_label.grid(row=2, column=2, padx=10)
